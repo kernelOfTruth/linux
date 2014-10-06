493d712255c3735e04941a6691b2dfd3a6631cda
kernelOfTruth <kerneloftruth@gmail.com>
2014-10-06 23:11:11 +0200

    Revert "[PATCH] workqueue: add quiescent state between work items"
    This reverts commit 3cd301f8ebb0daa365ab07c17b248343486ae7d2.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -2101,10 +2101,9 @@ __acquires(&pool->lock)
 	 * kernels, where a requeueing work item waiting for something to
 	 * happen could deadlock with stop_machine as such work item could
 	 * indefinitely requeue itself while all other CPUs are trapped in
-	 * stop_machine. At the same time, report a quiescent RCU state so
-	 * the same condition doesn't freeze RCU.
+	 * stop_machine.
 	 */
-	cond_resched_rcu_qs();
+	cond_resched();
 
 	spin_lock_irq(&pool->lock);
 
