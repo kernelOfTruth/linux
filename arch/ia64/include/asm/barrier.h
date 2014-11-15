f75cd5313660e73a41d4c50b4aca506de6d4b954
Alexander Duyck <alexander.h.duyck@redhat.com>
2014-11-15 20:06:08 +0100

    [PATCH 1/3] arch: Introduce load_acquire() and store_release()
    It is common for device drivers to make use of acquire/release semantics
    when dealing with descriptors stored in device memory.  On reviewing the
    documentation and code for smp_load_acquire() and smp_store_release() as
    well as reviewing an IBM website that goes over the use of PowerPC barriers
    at http://www.ibm.com/developerworks/systems/articles/powerpc.html it
    occurred to me that the same code could likely be applied to device drivers.
    
    As a result this patch introduces load_acquire() and store_release().  The
    load_acquire() function can be used in the place of situations where a test
    for ownership must be followed by a memory barrier.  The below example is
    from ixgbe:
    
    	if (!rx_desc->wb.upper.status_error)
    		break;
    
    	/* This memory barrier is needed to keep us from reading
    	 * any other fields out of the rx_desc until we know the
    	 * descriptor has been written back
    	 */
    	rmb();
    
    With load_acquire() this can be changed to:
    
    	if (!load_acquire(&rx_desc->wb.upper.status_error))
    		break;
    
    A similar change can be made in the release path of many drivers.  For
    example in the Realtek r8169 driver there are a number of flows that
    consist of something like the following:
    
    	wmb();
    
    	status = opts[0] | len | (RingEnd * !((entry + 1) % NUM_TX_DESC));
    	txd->opts1 = cpu_to_le32(status);
    
    	tp->cur_tx += frags + 1;
    
    	wmb();
    
    With store_release() this can be changed to the following:
    
    	status = opts[0] | len | (RingEnd * !((entry + 1) % NUM_TX_DESC));
    	store_release(&txd->opts1, cpu_to_le32(status));
    
    	tp->cur_tx += frags + 1;
    
    	wmb();
    
    The resulting assembler code generated as a result can be significantly
    less expensive on architectures such as x86 and s390 that support strong
    ordering.  On architectures that are able to use different primitives than
    their rmb/wmb() such as powerpc, ia64, and arm64 we should see gains as we
    are able to use less expensive barriers, and for other architectures we end
    up using a mb() which may come at the same amount of overhead or more than
    a rmb/wmb() as we must ensure Load/Store ordering.
    
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    Cc: Michael Ellerman <michael@ellerman.id.au>
    Cc: Michael Neuling <mikey@neuling.org>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: David Miller <davem@davemloft.net>
    Signed-off-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    ---
     arch/arm/include/asm/barrier.h      |   15 +++++++++
     arch/arm64/include/asm/barrier.h    |   59 ++++++++++++++++++-----------------
     arch/ia64/include/asm/barrier.h     |    7 +++-
     arch/metag/include/asm/barrier.h    |   15 +++++++++
     arch/mips/include/asm/barrier.h     |   15 +++++++++
     arch/powerpc/include/asm/barrier.h  |   24 +++++++++++---
     arch/s390/include/asm/barrier.h     |    7 +++-
     arch/sparc/include/asm/barrier_64.h |    6 ++--
     arch/x86/include/asm/barrier.h      |   22 ++++++++++++-
     include/asm-generic/barrier.h       |   15 +++++++++
     10 files changed, 144 insertions(+), 41 deletions(-)

--- arch/ia64/include/asm/barrier.h
+++ arch/ia64/include/asm/barrier.h
@@ -63,20 +63,23 @@
  * need for asm trickery!
  */
 
-#define smp_store_release(p, v)						\
+#define store_release(p, v)						\
 do {									\
 	compiletime_assert_atomic_type(*p);				\
 	barrier();							\
 	ACCESS_ONCE(*p) = (v);						\
 } while (0)
 
-#define smp_load_acquire(p)						\
+#define load_acquire(p)						\
 ({									\
 	typeof(*p) ___p1 = ACCESS_ONCE(*p);				\
 	compiletime_assert_atomic_type(*p);				\
 	barrier();							\
 	___p1;								\
 })
+
+#define smp_store_release(p, v)	store_release(p, v)
+#define smp_load_acquire(p)	load_acquire(p)
 
 /*
  * XXX check on this ---I suspect what Linus really wants here is
