f9c92ec53e020eaef44d681bf1fa997521ac66eb
kernelOfTruth <kerneloftruth@gmail.com>
2014-11-21 21:30:48 +0100

    Revert "[PATCH v5 1/4] mm/page_alloc: fix incorrect isolation behavior by rechecking migratetype"
    This reverts commit bd283f8609689caa86e030fc502bef6df9b48ad4.

--- mm/page_isolation.c
+++ mm/page_isolation.c
@@ -60,7 +60,6 @@ out:
 		int migratetype = get_pageblock_migratetype(page);
 
 		set_pageblock_migratetype(page, MIGRATE_ISOLATE);
-		zone->nr_isolate_pageblock++;
 		nr_pages = move_freepages_block(zone, page, MIGRATE_ISOLATE);
 
 		__mod_zone_freepage_state(zone, -nr_pages, migratetype);
@@ -84,7 +83,6 @@ void unset_migratetype_isolate(struct page *page, unsigned migratetype)
 	nr_pages = move_freepages_block(zone, page, migratetype);
 	__mod_zone_freepage_state(zone, nr_pages, migratetype);
 	set_pageblock_migratetype(page, migratetype);
-	zone->nr_isolate_pageblock--;
 out:
 	spin_unlock_irqrestore(&zone->lock, flags);
 }
