60ea3e9671ae7533101ad9be9090488e613951fa
Mahendran Ganesh <opensource.ganesh@gmail.com>
2014-12-06 21:06:08 +0100

    [PATCH v2] mm/zsmalloc: avoid duplicate assignment of prev_class
    In zs_create_pool(), prev_class is assigned (ZS_SIZE_CLASSES - 1)
    times. And the prev_class only references to the previous size_class.
    So we do not need unnecessary assignement.
    
    This patch assigns *prev_class* when a new size_class structure
    is allocated and uses prev_class to check whether the first class
    has been allocated.
    
    Signed-off-by: Mahendran Ganesh <opensource.ganesh@gmail.com>
    
    ---
    v1 -> v2:
      - follow Dan Streetman's advise to use prev_class to
        check whether the first class has been allocated
      - follow Minchan Kim's advise to remove uninitialized_var()
    ---
     mm/zsmalloc.c |    7 ++++---
     1 file changed, 4 insertions(+), 3 deletions(-)

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -983,7 +983,7 @@ struct zs_pool *zs_create_pool(gfp_t flags)
 		int size;
 		int pages_per_zspage;
 		struct size_class *class;
-		struct size_class *prev_class;
+		struct size_class *prev_class = NULL;
 
 		size = ZS_MIN_ALLOC_SIZE + i * ZS_SIZE_CLASS_DELTA;
 		if (size > ZS_MAX_ALLOC_SIZE)
@@ -999,8 +999,7 @@ struct zs_pool *zs_create_pool(gfp_t flags)
 		 * characteristics. So, we makes size_class point to
 		 * previous size_class if possible.
 		 */
-		if (i < ZS_SIZE_CLASSES - 1) {
-			prev_class = pool->size_class[i + 1];
+		if (prev_class) {
 			if (can_merge(prev_class, size, pages_per_zspage)) {
 				pool->size_class[i] = prev_class;
 				continue;
@@ -1016,6 +1015,8 @@ struct zs_pool *zs_create_pool(gfp_t flags)
 		class->pages_per_zspage = pages_per_zspage;
 		spin_lock_init(&class->lock);
 		pool->size_class[i] = class;
+
+		prev_class = class;
 	}
 
 	pool->flags = flags;
