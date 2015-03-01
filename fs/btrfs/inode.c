9c8646e982fab46745f92c13105a6bbda82c2691
Josef Bacik <jbacik@fb.com>
2015-03-01 03:39:53 +0100

    [PATCH] Btrfs: add metadump_v2 support to the kernel
    For newly restored metadumps we can actually mount the fs and use it properly
    except that the data obviously doesn't match properly.  To get around this make
    us skip csum validation if the metadump_v2 flag is set on the fs, this will
    allow us to reproduce balance issues with metadumps.  Thanks,
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>
    ---
     fs/btrfs/ctree.h | 1 +
     fs/btrfs/inode.c | 4 ++++
     2 files changed, 5 insertions(+)

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -3030,6 +3030,10 @@ static int btrfs_readpage_end_io_hook(struct btrfs_io_bio *io_bio,
 	if (BTRFS_I(inode)->flags & BTRFS_INODE_NODATASUM)
 		return 0;
 
+	if (btrfs_super_flags(root->fs_info->super_copy) &
+	    BTRFS_SUPER_FLAG_METADUMP_V2)
+		return 0;
+
 	if (root->root_key.objectid == BTRFS_DATA_RELOC_TREE_OBJECTID &&
 	    test_range_bit(io_tree, start, end, EXTENT_NODATASUM, 1, NULL)) {
 		clear_extent_bits(io_tree, start, end, EXTENT_NODATASUM,
