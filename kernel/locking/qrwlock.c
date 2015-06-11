16d90dd6ef4040e80b81913308252a74330e3c7e
Waiman Long <waiman.long@hp.com>
2015-06-11 20:57:42 +0200

    [PATCH v2 1/2] locking/qrwlock: Fix bug in interrupt handling code
    The qrwlock is fair in the process context, but becoming unfair when
    in the interrupt context to support use cases like the tasklist_lock.
    However, the unfair code in the interrupt context has problem that
    may cause deadlock.
    
    The fast path increments the reader count. In the interrupt context,
    the reader in the slowpath will wait until the writer release the
    lock. However, if other readers have the lock and the writer is just
    in the waiting mode. It will never get the write lock because the
    that interrupt context reader has increment the count. This will
    cause deadlock.
    
    This patch fixes this problem by checking the state of the
    reader/writer count retrieved at the fast path. If the writer
    is in waiting mode, the reader will get the lock immediately and
    return. Otherwise, it will wait until the writer release the lock
    like before.
    
    Signed-off-by: Waiman Long <Waiman.Long@hp.com>
    ---
     include/asm-generic/qrwlock.h |    4 ++--
     kernel/locking/qrwlock.c      |   14 ++++++++------
     2 files changed, 10 insertions(+), 8 deletions(-)

--- kernel/locking/qrwlock.c
+++ kernel/locking/qrwlock.c
@@ -43,22 +43,24 @@ rspin_until_writer_unlock(struct qrwlock *lock, u32 cnts)
  * queue_read_lock_slowpath - acquire read lock of a queue rwlock
  * @lock: Pointer to queue rwlock structure
  */
-void queue_read_lock_slowpath(struct qrwlock *lock)
+void queue_read_lock_slowpath(struct qrwlock *lock, u32 cnts)
 {
-	u32 cnts;
-
 	/*
 	 * Readers come here when they cannot get the lock without waiting
 	 */
 	if (unlikely(in_interrupt())) {
 		/*
-		 * Readers in interrupt context will spin until the lock is
-		 * available without waiting in the queue.
+		 * Readers in interrupt context will get the lock immediately
+		 * if the writer is just waiting (not holding the lock yet)
+		 * or they will spin until the lock is available without
+		 * waiting in the queue.
 		 */
-		cnts = smp_load_acquire((u32 *)&lock->cnts);
+		if ((cnts & _QW_WMASK) != _QW_LOCKED)
+			return;
 		rspin_until_writer_unlock(lock, cnts);
 		return;
 	}
+
 	atomic_sub(_QR_BIAS, &lock->cnts);
 
 	/*
