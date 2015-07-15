87e25fdaf1ee2d63b2f39972a055dc1730d5e513
Con Kolivas <kernel@kolivas.org>
2015-07-15 21:23:50 +0200

    bfs462-nosubmit

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -3573,28 +3573,12 @@ rerun_prev_unlocked:
 	sched_preempt_enable_no_resched();
 }
 
-static inline void sched_submit_work(struct task_struct *tsk)
-{
-	if (!tsk->state || tsk_is_pi_blocked(tsk))
-		return;
-	/*
-	 * If we are going to sleep and we have plugged IO queued,
-	 * make sure to submit it to avoid deadlocks.
-	 */
-	if (blk_needs_flush_plug(tsk))
-		blk_schedule_flush_plug(tsk);
-}
-
 asmlinkage __visible void __sched schedule(void)
 {
-	struct task_struct *tsk = current;
-
-	sched_submit_work(tsk);
 	do {
 		__schedule();
 	} while (need_resched());
 }
-
 EXPORT_SYMBOL(schedule);
 
 #ifdef CONFIG_CONTEXT_TRACKING
