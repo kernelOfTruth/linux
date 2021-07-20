60d21494639eee293ca9e7388643f120f0d2ab10
kernelOfTruth <kerneloftruth@gmail.com>
2021-07-20 21:55:48 +0200

    Revert "futex2: Implement support for different futex sizes"
    This reverts commit 1fca73f24c71bf7dce0bbb3d2f7b547335248da1.

--- include/uapi/linux/futex.h
+++ include/uapi/linux/futex.h
@@ -42,10 +42,7 @@
 					 FUTEX_PRIVATE_FLAG)
 
 /* Size argument to futex2 syscall */
-#define FUTEX_8		0
-#define FUTEX_16	1
 #define FUTEX_32	2
-#define FUTEX_64	3
 
 #define FUTEX_SIZE_MASK	0x3
 
