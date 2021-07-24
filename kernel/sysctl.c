c53b3b310996ffec23b09c8e4f1b0e62c33207a2
kernelOfTruth <kerneloftruth@gmail.com>
2021-07-24 02:15:42 +0200

    kernel/sysctl.c: 	fix up zero & one in hrtimer/hrtimeout bits, 			move it out of CONFIG_SCHED_ALT
    Signed-off-by: kernelOfTruth <kerneloftruth@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -123,8 +123,9 @@ static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
 static int two_hundred = 200;
 static int one_thousand = 1000;
-#ifdef CONFIG_SCHED_ALT
 static int __maybe_unused zero = 0;
+static int __maybe_unused one = 1;
+#ifdef CONFIG_SCHED_ALT
 extern int sched_yield_type;
 #endif
 extern int hrtimer_granularity_us;
